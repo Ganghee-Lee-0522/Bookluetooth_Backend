591997397907ef838bf069c1ebdf6f257adfd745
lildev1-admin <skap0329@naver.com>
2023-01-31 03:40:49 +0900

    Bookluetooth 프로젝트 시작

---
+++ bookluetooth/src/test/java/app/cotato/bookluetooth/BookluetoothApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cotato.bookluetooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookluetoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
